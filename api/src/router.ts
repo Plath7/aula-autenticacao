ecc29fb97daa52d92f265a7e38e0c5627accd90b
Igor <igorplath.martins.stz@gmail.com>
2024-10-09 15:59:49 -0300

    aula-autenticaca

--- api/src/router.ts
+++ api/src/router.ts
@@ -1,12 +1,17 @@
 import express, { Request, Response } from 'express';
 import { Consultas } from './Consultas';
+import { Auth } from './util/Auth';
+
 
 const router = express.Router();
 
+router.post('/login', Auth.validacaoUsuario);
+
+router.get('/rota-protegida', Auth.verifyToken, (req: Request, res: Response) => { res.send('Rota protegida, se você está vendo essa mensagem é porque está autenticado no sistema') });
+
 router.get('/', (req: Request, res: Response) => { res.send('Hello World!') });
 
-router.get('/rota-protegida', (req: Request, res: Response) => { res.send('Rota protegida, se você está vendo essa mensagem é porque está autenticado no sistema') });
+router.get('/pessoas', Auth.verifyToken, Consultas.todos);
 
-router.get('/pessoas', Consultas.todos);
 
 export { router }
--- interface/src/components/Navegacao/Navegacao.jsx
+++ interface/src/components/Navegacao/Navegacao.jsx
@@ -1,6 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/esm/Button';
 
 function Navegacao() {
 
@@ -19,6 +20,9 @@ function Navegacao() {
                     <Navbar.Brand href="/" style={estiloNavOptions}>Home</Navbar.Brand>
                     <Nav className="me-auto">
                         <Nav.Link href="/pessoas" style={estiloNavOptions}>Pessoas</Nav.Link>
+                        <Button href='/login' variant='light'>Login</Button>
+                        <Button href='/login' variant='light'>Logout</Button>
+
                     </Nav>
                 </Container>
             </Navbar>
